160f20e75c97dde3a38140d0d5f7d4fe5ac480ea
ClaytonBazar <claytonbazar04@gmail.com>
2023-11-04 17:03:28 +0200

    front page

--- index.php
+++ index.php
@@ -4,37 +4,132 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Gestao de Bibliotecas</title>
+    <title>Books4You</title>
 </head>
 <body>
-    <header>
-        <div class="logo"></div>
-        <input type="search" name="" id="">
-        <nav>
-            <ul>
-                <li><a href="">HOME</a></li>
-                <li><a href="">CLIENTE</a></li>
-                <li><a href="">AJUDA</a></li>
-                <li><a href="">COMPRAS</a></li>
-                <li><a href="">ESTA</a></li>
+    <header class="header">
+        <a href="#" class="logo"></a>
+        <nav class="navbar">
+            <ul >
+                <li><a class="home" href="#">HOME</a></li>
+                <li><a href="#">CLIENTE</a></li>
+                <li><a href="#">AJUDA</a></li>
+                <li><a href="#">COMPRAS</a></li>
+                <li><a href="#">LISTA</a></li>
             </ul>
         </nav>
     </header>
     <main class="container-main">
         <div class="sponsors">
-
+            sedkfjsnedvfsejf
         </div>
         <div class="destaques">
-
+        <h2>Destaques</h2>
+            <a href="">
+                <div class="card">
+                    <img src="imagens/livros/Anjos e Demônios (Angels & Demons).jpeg" alt="">
+                    <h3>Titulu 3</h3>
+                </div>
+            </a>
+            <a href="">
+                <div class="card">
+                <img src="imagens/livros/Jujutsu Kaisen.jpeg" alt="">
+                <h3>Titulu 3</h3>
+                </div>
+            </a>
+            <a href="">
+                <div class="card">
+                <img src="imagens/livros/A menina das estrelas.jpeg" alt="">
+                <h3>Titulu 3</h3>
+                </div>
+            </a>
+            <a href="">
+                <div class="card">
+                <img src="imagens/livros/Clássicos da Literatura - Mundo de Livros.jpeg" alt="">
+                <h3>Titulu 3</h3>
+                </div>
+            </a>
         </div>
-        <div class="vendidos">
-
+        <div class="destaques">
+            <h2>Mais Vendidos</h2>
+            <a href="">
+                <div class="card">
+                    <img src="imagens/livros/Mushoku Tensei – Anime revela Vídeo Teaser Promo.jpeg" alt="">
+                    <h3>Titulu 3</h3>
+                </div>
+            </a>
+            <a href="">
+                <div class="card">
+                <img src="imagens/livros/O Voo do Pintarriscos.jpeg" alt="">
+                <h3>Titulu 3</h3>
+                </div>
+            </a>
+            <a href="">
+                <div class="card">
+                <img src="imagens/livros/Pai Rico, Pai Pobre - 8 lições fantásticas que o livro pode lhe ensinar.jpeg" alt="">
+                <h3>Titulu 3</h3>
+                </div>
+            </a>
+            <a href="">
+                <div class="card">
+                <img src="imagens/livros/Tokyo Revengers.jpeg" alt="">
+                <h3>Titulu 3</h3>
+                </div>
+            </a>
         </div>
-        <div class="pre-lancamentos">
-
+        <div class="destaques">
+            <h2>Pre-Lancamento</h2>
+            <a href="">
+                <div class="card">
+                    <img src="imagens/livros/Capa para ebook, nós desenvolvemos sempre o melhor para seus negócios_.jpeg" alt="">
+                    <h3>Titulu 3</h3>
+                </div>
+            </a>
+            <a href="">
+                <div class="card">
+                <img src="imagens/livros/Capa sociologia.jpeg" alt="">
+                <h3>Titulu 3</h3>
+                </div>
+            </a>
+            <a href="">
+                <div class="card">
+                <img src="imagens/livros/Revolução Industrial Capa.jpeg" alt="">
+                <h3>Titulu 3</h3>
+                </div>
+            </a>
+            <a href="">
+                <div class="card">
+                <img src="imagens/livros/O Lado Feio do Amor, Colleen Hoover.jpeg" alt="">
+                <h3>Titulu 3</h3>
+                </div>
+            </a>
         </div>
-        <div class="novidades">
-
+        <div class="destaques">
+            <h2>Novidades</h2>
+            <a href="">
+                <div class="card">
+                    <img src="imagens/livros/Lilith.jpeg" alt="">
+                    <h3>Titulo 1</h3>
+                </div>
+            </a>
+            <a href="">
+                <div class="card">
+                <img src="imagens/livros/Frases sobre criatividade 🌟.jpeg" alt="">
+                <h3>Titulo 2</h3>
+                </div>
+            </a>
+            <a href="">
+                <div class="card">
+                <img src="imagens/livros/Ler é poder viver outras emoções na própria imaginação_.jpeg" alt="">
+                <h3>Titulu 3</h3>
+                </div>
+            </a>
+            <a href="">
+                <div class="card">
+                <img src="imagens/livros/Goodreads.jpeg" alt="">
+                <h3>Titulu 3</h3>
+                </div>
+            </a>
         </div>
     </main>
     <aside>
