826bb882d7f168dffa4fcbed77529884d6ef26ba
ClaytonBazar <claytonbazar04@gmail.com>
2023-11-04 02:34:45 +0200

    Imagens

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Gestao de Bibliotecas</title>
+</head>
+<body>
+    <header>
+        <div class="logo"></div>
+        <input type="search" name="" id="">
+        <nav>
+            <ul>
+                <li><a href="">CLIENTE</a></li>
+                <li><a href="">AJUDA</a></li>
+                <li><a href="">COMPRAS</a></li>
+                <li><a href="">ESTA</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main class="container-main">
+        <div class="sponsors">
+
+        </div>
+        <div class="destaques">
+
+        </div>
+        <div class="vendidos">
+
+        </div>
+        <div class="pre-lancamentos">
+
+        </div>
+        <div class="novidades">
+
+        </div>
+    </main>
+    <aside>
+        <div class="destaques">
+
+        </div>
+        <div class="livros">
+
+        </div>
+        <div class="ebooks">
+
+        </div>
+        <div class="BD-Manga"></div>
+    </aside>
+    <footer class="container-footer">
+
+    </footer>
+</body>
+</html>
